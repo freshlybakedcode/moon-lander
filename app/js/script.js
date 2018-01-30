c9c2908cbd5f2c658184186daf0439672d2c21db
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2018-01-30 22:41:16 +0000

    A vague hint of user control

--- app/js/script.js
+++ app/js/script.js
@@ -3,18 +3,39 @@ var ctx = canvas.getContext('2d');
 
 let x = 155;
 let y = 30;
+const gravityStrength = 1;
 
-var drawSpaceship = function () {
-	//Nose
-	ctx.fillStyle = '#fff';
-	ctx.beginPath();
-	ctx.moveTo(x, y);
-	ctx.lineTo(x - 6, y + 6);
-	ctx.lineTo(x + 6, y + 6);
-	ctx.fill();
-	//Body
-	ctx.fillStyle = '#fff';
-	ctx.fillRect(x - 6, y + 6, 12, 12);
+const spaceship = {
+	draw: function (color) {
+		//Nose
+		ctx.fillStyle = color;
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.lineTo(x - 6, y + 6);
+		ctx.lineTo(x + 6, y + 6);
+		ctx.fill();
+		//Body
+		ctx.fillStyle = color;
+		ctx.fillRect(x - 6, y + 6, 12, 12);
+	},
+	applyGravity: function () {
+		y += gravityStrength;
+	}
 }
 
-drawSpaceship();
+document.body.onkeydown = function (e) {
+	if (e.keyCode == 32) {
+		console.log('onKeyDown');
+		y -= 1;
+	}
+}
+
+var interval = setInterval(function () {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	spaceship.applyGravity();
+	spaceship.draw('#fff');
+}, 100);
+
+// setTimeout(function() {
+//   clearInterval(interval);
+// });
