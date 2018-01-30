e4a30c7bb67814723ffdba553e13c9bc46edc72f
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2018-01-30 22:16:48 +0000

    Draw a little ship and have axis controls

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,20 @@
-console.log(`We got a script here.`);
+var canvas = document.getElementById('universe');
+var ctx = canvas.getContext('2d');
+
+let x = 155;
+let y = 30;
+
+var drawSpaceship = function () {
+	//Nose
+	ctx.fillStyle = '#fff';
+	ctx.beginPath();
+	ctx.moveTo(x, y);
+	ctx.lineTo(x - 6, y + 6);
+	ctx.lineTo(x + 6, y + 6);
+	ctx.fill();
+	//Body
+	ctx.fillStyle = '#fff';
+	ctx.fillRect(x - 6, y + 6, 12, 12);
+}
+
+drawSpaceship();
