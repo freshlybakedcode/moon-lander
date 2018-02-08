6ecb69ff274af877cb16ca9ccb5ab2b6a096c469
James Brenton <james@jamesbrenton.co.uk>
2018-02-08 11:35:38 +0000

    Implemented game loop for detecting key presses

--- app/js/script.js
+++ app/js/script.js
@@ -3,7 +3,9 @@ var ctx = canvas.getContext('2d');
 
 let x = 155;
 let y = 30;
-const gravityStrength = 1;
+let keyState = {};    
+const gravityStrength = 0.3;
+const windStrength = 0.1;
 
 const spaceship = {
 	draw: function (color) {
@@ -20,21 +22,35 @@ const spaceship = {
 	},
 	applyGravity: function () {
 		y += gravityStrength;
+	},
+	applyWind: function () {
+		x += windStrength;
 	}
 }
 
-document.body.onkeydown = function (e) {
-	if (e.keyCode == 32) {
-		console.log('onKeyDown');
-		y -= 1;
-	}
-}
+window.addEventListener('keydown',function(e){
+    keyState[e.keyCode || e.which] = true;
+},true);    
+window.addEventListener('keyup',function(e){
+    keyState[e.keyCode || e.which] = false;
+},true);
 
 var interval = setInterval(function () {
+	if (keyState[32]) {
+		y -= 2;
+	}
+	if (keyState[37] || keyState[65]){
+        x -= 1;
+    }    
+    if (keyState[39] || keyState[68]){
+        x += 1;
+    }
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	spaceship.applyGravity();
+	spaceship.applyWind();
 	spaceship.draw('#fff');
-}, 100);
+}, 10);
 
 // setTimeout(function() {
 //   clearInterval(interval);
