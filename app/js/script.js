97ac89594936dfe7ecec50e6b3b5cdc530539d8d
James Brenton <james@jamesbrenton.co.uk>
2018-02-08 12:16:11 +0000

    Stat output

--- app/js/script.js
+++ app/js/script.js
@@ -4,10 +4,15 @@ var ctx = canvas.getContext('2d');
 let x = 155;
 let y = 30;
 let keyState = {};    
-const gravityStrength = 0.3;
-const windStrength = 0.1;
+const gravityStrength = 1.001;
+const windStrength = 0.01;
+const rocketPower = 0.1;
+
+let speed = document.querySelector('.speed');
+let fuel = document.querySelector('.fuel');
 
 const spaceship = {
+	fuel: 500,
 	draw: function (color) {
 		//Nose
 		ctx.fillStyle = color;
@@ -21,12 +26,17 @@ const spaceship = {
 		ctx.fillRect(x - 6, y + 6, 12, 12);
 	},
 	applyGravity: function () {
-		y += gravityStrength;
+		y = y * gravityStrength;
 	},
 	applyWind: function () {
 		x += windStrength;
 	}
-}
+};
+
+var updateStats = function() {
+	speed.textContent = `Speed: ${y.toFixed(1)}`;
+	fuel.textContent = `Fuel: ${spaceship.fuel}`;
+};
 
 window.addEventListener('keydown',function(e){
     keyState[e.keyCode || e.which] = true;
@@ -36,13 +46,14 @@ window.addEventListener('keyup',function(e){
 },true);
 
 var interval = setInterval(function () {
-	if (keyState[32]) {
-		y -= 2;
+	if (keyState[32]) {	//spacebar
+		y -= rocketPower;
+		spaceship.fuel--;
 	}
-	if (keyState[37] || keyState[65]){
+	if (keyState[37] || keyState[65]){	//left
         x -= 1;
     }    
-    if (keyState[39] || keyState[68]){
+    if (keyState[39] || keyState[68]){	//right
         x += 1;
     }
 
@@ -50,6 +61,7 @@ var interval = setInterval(function () {
 	spaceship.applyGravity();
 	spaceship.applyWind();
 	spaceship.draw('#fff');
+	updateStats();
 }, 10);
 
 // setTimeout(function() {
